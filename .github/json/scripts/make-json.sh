132f69fcbc300ae5846c88781a89ec9de2e0b81f
Federico Iacobucci <85311401+Kaanis@users.noreply.github.com>
2023-09-16 20:16:53 +0200

    translated make-json.sh

--- .github/json/scripts/make-json.sh
+++ .github/json/scripts/make-json.sh
@@ -1,87 +1,87 @@
 #!/bin/bash
-# Script to create the Json checklist file for WSTG
+# Script per creare il file Json della WSTG
 
 function join { local IFS="$1"; shift; echo "$*"; }
 
 cd document/4-Web_Application_Security_Testing
 
-# Start the json string
+# Inizializza la stringa json
 echo "{" > checklist.json
-# Open categories
+# Categorie aperte
 echo "    \"categories\": {"  >> checklist.json
-# Iterate all category folders to get the categories
+# Itera tutte le cartelle di categoria per ottenere le categorie
 for d in */ ; do
-    # Split the folder name to get category and category number
-    # Select folders start form 01
+    # Divide il nome della cartella per ottenere la categoria e il numero di categoria
+    # Selezionare le cartelle a partire dalla forma 01
     while IFS='-' read -ra FOLD; do
         if [ ${FOLD[0]} -gt 0 ]
         then
-            # In case of multiple `-` in the category name
-            # Join the sections other than the first
+            # Nel caso di multeplici nel nome di categoria
+            # Unire le sezioni diverse dalla prima
             if [ ${#FOLD[@]} -gt 2 ];then
                 category=$(join - ${FOLD[@]:1})
             else
                 category=${FOLD[1]}
             fi
-            # Start category sub section
-            # Add coma from the second entry of the list
+            # Inizializza la sottosezione della categoria
+            # Aggiunge la virgola dalla seconda voce dell'elenco
             if [ ${FOLD[0]} -gt 1 ];then
                 echo "        ,\"${category%?}\": {" | tr '_' ' '  >> checklist.json
             else
                 echo "        \"${category%?}\": {" | tr '_' ' '  >> checklist.json
             fi
-            # Get category ID from the first file
+            # Ottiene l'ID della categoria dal primo file
             cid=`cat $d/01-* | grep "|WSTG-.*" | cut -d "|" -f 2 | sed 's/-01//'`
-            # Add category ID and start tests
+            # Aggiunge l'ID della categoria e avvia i test
             echo "            \"id\":\"${cid}\","  >> checklist.json
             echo "            \"tests\":["  >> checklist.json
             count=0
             for file in $d*.md; do
-                # Remove README
+                # Rimuove README
                 if [[ $file != *"README.md" ]];then
-                    # Get test ID
+                    # Ottiene l'ID del test
                     tid=`cat $file | grep "|WSTG-.*" | cut -d "|" -f 2`
-                    # If test id exists
+                    # Se l'id del test esiste
                     if [ ! -z "$tid" ];then
-                        # Add coma from the second entry of the list
+                        # Aggiungere la virgola dalla seconda voce dell'elenco
                         if [ $count -gt 0 ];then
                             echo "                ,{"  >> checklist.json
                         else
                             echo "                {"  >> checklist.json
                         fi
-                        # Get Objective of the test from the file
+                        # Ottiene l'obiettivo del test dal file
                         objectiveString=`awk "/## Test Objectives/{flag=1; next} /## /{flag=0} flag" $file |  awk 'NF'`
                         objcount=0
                         objectives=()
-                        # Convert Objective string to list of objectives
+                        # Converte la stringa dell'obiettivo in un elenco di obiettivi
                         while read line;
                         do
                             objectives[$objcount]=`echo ${line//[$'\t\r\n']} |  cut -c 3-`
                             objcount=$((objcount+1))
                         done <<< "$objectiveString"
 
-                        # Get test name and reference link from the file
+                        # Ottiene il nome del test e il link di riferimento dal file
                         read -r tname < $file
                         tname=${tname:2}
                         tref=`echo $file | sed 's/.md//'`
-                        # Add test ID, test name and reference link from the file
+                        # Aggiunge l'ID del test, il nome del test e il link di riferimento dal file.
                         echo "                \"name\":\"${tname}\","  >> checklist.json
                         echo "                \"id\":\"${tid}\","  >> checklist.json
                         echo "                \"reference\":\"https://owasp.org/www-project-web-security-testing-guide/latest/4-Web_Application_Security_Testing/$tref\","  >> checklist.json
                         echo "                \"objectives\":["  >> checklist.json
-                        # Add objective array
+                        # Aggiunge l'array di obiettivi
                         objcount=0
                         for objective in "${objectives[@]}"
                         do
                             objcount=$((objcount+1))
-                            # Check for the last entry of Objective and remove coma
+                            # Controlla l'ultima voce dell'Obiettivo e rimuovere la virgola
                             if [ ${#objectives[@]} -eq $objcount ];then
                                 echo "                    \"${objective}\""  >> checklist.json
                             else
                                 echo "                    \"${objective}\","  >> checklist.json
                             fi
                         done
-                        # Close Objective list
+                        # Chiude l'elenco degli obiettivi
                         echo "                  ]"  >> checklist.json
 
                         echo "                }"  >> checklist.json
@@ -89,18 +89,18 @@ for d in */ ; do
                     fi
                 fi
             done
-            # Close tests list
+            # Chiude l'elenco dei test
             echo "            ]"  >> checklist.json
-            # Close category sub section
+            # Chiude la sottosezione della categoria
             echo "        }"  >> checklist.json
         fi
     done <<< "$d"
 done
-# Close categories
+# Chiude le categorie
 echo "    }"  >> checklist.json
-# End Json string
+# Conclude la stringa Json
 echo "}"  >> checklist.json
 cat checklist.json
 
-# Move generated file to checklists folder
+# Sposta il file generato nella cartella delle liste di controllo
 mv checklist.json ../../checklists/.
